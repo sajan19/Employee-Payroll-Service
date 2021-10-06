3e1f67b9427d4270949ba0b8e347ae5a3f1439a1
Sajan <sajanmhatre29@gmail.com>
2021-10-07 00:46:22 +0530

    Print Employee Payroll

--- src/main/java/EmployeePayrollFileIOService.java
+++ src/main/java/EmployeePayrollFileIOService.java
@@ -32,4 +32,12 @@ public class EmployeePayrollFileIOService {
         }
         return entries;
     }
+    // method to print the lines in the file
+    public void printData() {
+        try {
+            Files.lines(new File("payroll-file.txt").toPath()).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/EmployeePayrollService.java
+++ src/main/java/EmployeePayrollService.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// To implement Employee Payroll Service System
+
 public class EmployeePayrollService {
     public List<EmployeePayrollData> employeePayrollList;
 
@@ -23,16 +25,12 @@ public class EmployeePayrollService {
         employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
     }
 
-    /* This method is implementing Welcome Message */
+// This method is implementing Welcome Message
     public void printWelcomeMessage() {
         System.out.println("Welcome To The Employee Payroll Service System");
     }
 
-    /**
-     * Purpose : To read the information of Employees from the console
-     *
-     * @param consoleInputReader takes the information of employees
-     */
+    //Method to read Employee Payroll Data
     private void readEmployeePayrollData(Scanner consoleInputReader) {
         System.out.println("Enter Employee ID : ");
         int id = consoleInputReader.nextInt();
@@ -44,7 +42,7 @@ public class EmployeePayrollService {
         employeePayrollList.add(new EmployeePayrollData(id, name, salary));
     }
 
-    /* This method is implementing to write the Employee Payroll to the console */
+//     Method to write the Employee Payroll to the console
     public void writeEmployeePayrollData(IOService ioService) {
         if (ioService.equals(IOService.CONSOLE_IO))
             System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrollList);
@@ -52,11 +50,16 @@ public class EmployeePayrollService {
             new EmployeePayrollFileIOService().writeData(employeePayrollList);
         }
     }
-
-    /* This method is implementing to count entries in a file */
+    //Method to Count Number of Entries
     public long countEntries(IOService ioService) {
         if (ioService.equals(IOService.FILE_IO))
             return new EmployeePayrollFileIOService().countEntries();
         return 0;
+    }
+
+//  Method to print the lines in the file
+    public void printData(IOService ioService) {
+        if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().printData();
     }
 }
